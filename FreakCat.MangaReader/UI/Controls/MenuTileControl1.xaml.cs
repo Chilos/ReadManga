215496fc001425c7d59a9f787b9575b838e93bd4
fm050629 <fm050629@gmailcom>
2016-02-11 00:15:42 +0300

    +Паралакс в каталоге Изменен дизайн тайлов каталоге

---
+++ FreakCat.MangaReader/UI/Controls/MenuTileControl1.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using FreakCat.MangaReader.ViewModel;
+using System.Windows.Input;
+using FreakCat.MangaReader.Model.Entities;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace FreakCat.MangaReader.UI.Controls
+{
+    public sealed partial class MenuTileControl1 : UserControl
+    {
+        public TileInfo Tile => DataContext as TileInfo;
+
+        public MenuTileControl1()
+        {
+            this.InitializeComponent();
+            this.DataContextChanged += (s, e) => Bindings.Update();
+        }
+
+
+        public static readonly DependencyProperty CommandParameterProperty = DependencyProperty.Register("CommandParameter", typeof(object), typeof(MenuTileControl1), new PropertyMetadata(0));
+        public object CommandParameter
+        {
+            get { return (object)GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+
+        public ICommand Command
+        {
+            get { return (ICommand)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register("Command", typeof(ICommand), typeof(MenuTileControl1), new PropertyMetadata(null));
+
+
+    }
+}
