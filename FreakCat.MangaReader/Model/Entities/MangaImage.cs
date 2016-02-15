d5dc5b435a4dbae507e53bc5a5168d3ab7f89a62
fm050629 <fm050629@gmailcom>
2016-02-16 00:21:25 +0300

    +Сущность для страниц чтения +Редизайн страницы чтения

---
+++ FreakCat.MangaReader/Model/Entities/MangaImage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace FreakCat.MangaReader.Model.Entities
+{
+    public class MangaImage
+    {
+        public BitmapImage Image { get; set; }
+        public string Title { get; set; }
+        public string PageNamber { get; set; }
+        public string Chapter { get; set; }
+    }
+}
--- FreakCat.MangaReader/ViewModel/Page3ViewModel.cs
+++ FreakCat.MangaReader/ViewModel/Page3ViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Imaging;
+using FreakCat.MangaReader.Model.Entities;
 using FreakCat.MangaReader.UI;
 using FreakCat.MangaReader.UI.Navigate;
 using GalaSoft.MvvmLight;
@@ -19,10 +24,20 @@ namespace FreakCat.MangaReader.ViewModel
         private ICommand _goBack;
         private ICommand _onLoadCommand;
         private readonly INavigationService _navigationService;
+        public ObservableCollection<MangaImage> ImageCollection { get; set; }
+
         public Page3ViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
-            
+            ImageCollection = new ObservableCollection<MangaImage>()
+            {
+                new MangaImage() {Title = "#shin5 - Kekkonshite mo Koishiteru", Image = new BitmapImage(new Uri(@"http://img2.mangachan.ru/manganew_webp/-9new/f/1455435592_fuuka_v10_ch95/02._.webp")), Chapter = "Том 10 Глава 95", PageNamber = "1/4"},
+                new MangaImage() {Title = "#shin5 - Kekkonshite mo Koishiteru", Image = new BitmapImage(new Uri(@"http://img2.mangachan.ru/manganew_webp/-9new/f/1455435592_fuuka_v10_ch95/03._.webp")), Chapter = "Том 10 Глава 95", PageNamber = "2/4"},
+                new MangaImage() {Title = "#shin5 - Kekkonshite mo Koishiteru", Image = new BitmapImage(new Uri(@"http://img2.mangachan.ru/manganew_webp/-9new/f/1455435592_fuuka_v10_ch95/04._.webp")), Chapter = "Том 10 Глава 95", PageNamber = "3/4"},
+                new MangaImage() {Title = "#shin5 - Kekkonshite mo Koishiteru", Image = new BitmapImage(new Uri(@"http://img2.mangachan.ru/manganew_webp/-9new/f/1455435592_fuuka_v10_ch95/05._.webp")), Chapter = "Том 10 Глава 95", PageNamber = "4/4"},
+            };
+
+
         }
         public ICommand GoBack
         {
