06ca0da5d5081432b4e47e250e182c5f053673d8
fm050629 <fm050629@gmailcom>
2016-02-16 02:13:25 +0300

    +поддержка webp

--- FreakCat.MangaReader/Model/Entities/MangaImage.cs
+++ FreakCat.MangaReader/Model/Entities/MangaImage.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
+using GalaSoft.MvvmLight;
 
 namespace FreakCat.MangaReader.Model.Entities
 {
     public class MangaImage
     {
-        public BitmapImage Image { get; set; }
+        public WriteableBitmap Image { get; set; }
         public string Title { get; set; }
         public string PageNamber { get; set; }
         public string Chapter { get; set; }
+
     }
 }
--- FreakCat.MangaReader/ViewModel/Page3ViewModel.cs
+++ FreakCat.MangaReader/ViewModel/Page3ViewModel.cs
@@ -4,9 +4,12 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Windows.Graphics.Imaging;
+using Windows.Storage.Streams;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -16,6 +19,8 @@ using FreakCat.MangaReader.UI;
 using FreakCat.MangaReader.UI.Navigate;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using Universal.WebP;
+using WinRTXamlToolkit.Imaging;
 
 namespace FreakCat.MangaReader.ViewModel
 {
@@ -23,21 +28,46 @@ namespace FreakCat.MangaReader.ViewModel
     {
         private ICommand _goBack;
         private ICommand _onLoadCommand;
+        private MangaImage _selectedImage;
         private readonly INavigationService _navigationService;
         public ObservableCollection<MangaImage> ImageCollection { get; set; }
+
+        public async Task<WriteableBitmap> qqqq(string url)
+        {
+            var bytes =  new HttpClient().GetByteArrayAsync(url).Result;
+
+            // Create an instance of the decoder
+            var webp = new WebPDecoder();
+
+            // Decode to BGRA (Bitmaps use this format)
+            var pixelData1 = await webp.DecodeBgraAsync(bytes);
+            var pixelData = pixelData1.ToArray();
+            // Get the size
+            var size = await webp.GetSizeAsync(bytes);
+
+            // With the size of the webp, create a WriteableBitmap
+            var bitmap = new WriteableBitmap((int)size.Width, (int)size.Height);
+
+            // Write the pixel data to the buffer
+            var stream = bitmap.PixelBuffer.AsStream();
+            await stream.WriteAsync(pixelData, 0, pixelData.Length);
+            return bitmap;
+        }
 
         public Page3ViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
-            ImageCollection = new ObservableCollection<MangaImage>()
+            ImageCollection = new ObservableCollection<MangaImage>();
+        }
+
+        public MangaImage SelectedImage
+        {
+            get { return _selectedImage; }
+            set
             {
-                new MangaImage() {Title = "#shin5 - Kekkonshite mo Koishiteru", Image = new BitmapImage(new Uri(@"http://img2.mangachan.ru/manganew_webp/-9new/f/1455435592_fuuka_v10_ch95/02._.webp")), Chapter = "Том 10 Глава 95", PageNamber = "1/4"},
-                new MangaImage() {Title = "#shin5 - Kekkonshite mo Koishiteru", Image = new BitmapImage(new Uri(@"http://img2.mangachan.ru/manganew_webp/-9new/f/1455435592_fuuka_v10_ch95/03._.webp")), Chapter = "Том 10 Глава 95", PageNamber = "2/4"},
-                new MangaImage() {Title = "#shin5 - Kekkonshite mo Koishiteru", Image = new BitmapImage(new Uri(@"http://img2.mangachan.ru/manganew_webp/-9new/f/1455435592_fuuka_v10_ch95/04._.webp")), Chapter = "Том 10 Глава 95", PageNamber = "3/4"},
-                new MangaImage() {Title = "#shin5 - Kekkonshite mo Koishiteru", Image = new BitmapImage(new Uri(@"http://img2.mangachan.ru/manganew_webp/-9new/f/1455435592_fuuka_v10_ch95/05._.webp")), Chapter = "Том 10 Глава 95", PageNamber = "4/4"},
-            };
-
-
+                _selectedImage = value;
+                RaisePropertyChanged(() => SelectedImage);
+            }
         }
         public ICommand GoBack
         {
@@ -55,11 +85,31 @@ namespace FreakCat.MangaReader.ViewModel
         {
             get
             {
-                return _onLoadCommand ?? (_onLoadCommand = new RelayCommand(() =>
+                return _onLoadCommand ?? (_onLoadCommand = new RelayCommand( async () =>
                 {
-                    var tmp = _navigationService.CurrentFrame;
-                    var view = ApplicationView.GetForCurrentView();
-                    view.TryEnterFullScreenMode();
+                    //var tmp = _navigationService.CurrentFrame;
+                    //var view = ApplicationView.GetForCurrentView();
+                    //view.TryEnterFullScreenMode();
+                    ImageCollection.Add(new MangaImage()
+                    {
+                        Title = "#shin5 - Kekkonshite mo Koishiteru",
+                        Image =
+                            await
+                                qqqq(
+                                    @"http://img2.mangachan.ru/manganew_webp/-9new/f/1455435592_fuuka_v10_ch95/01._.webp"),
+                        Chapter = "Том 10 Глава 95",
+                        PageNamber = "1/4"
+                    });
+                    ImageCollection.Add(new MangaImage()
+                    {
+                        Title = "#shin5 - Kekkonshite mo Koishiteru",
+                        Image =
+                            await
+                                qqqq(
+                                    @"http://img2.mangachan.ru/manganew_webp/-9new/f/1455435592_fuuka_v10_ch95/02._.webp"),
+                        Chapter = "Том 10 Глава 95",
+                        PageNamber = "2/4"
+                    });
 
                 }));
             }
